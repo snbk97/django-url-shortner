b69b3abb209729b41bef3d087f340b5b4dae8301
Sayan Bhowmik <snbk97@gmail.com>
2018-07-19 16:50:21 +0530

    Temp DEBUG

--- tiny/settings/production.py
+++ tiny/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ql=_r$j-nr%n%54g*k1v1_%q=@5ej_!7%iv0o7k$-2qmx^$z2h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', "smolr.herokuapp.com/"]
 SITE_URL = "http://smolr.herokuapp.com/"

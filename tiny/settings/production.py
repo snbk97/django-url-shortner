12cb55f0e785ab584b93bf51ba250ac40d363fd7
Sayan Bhowmik <snbk97@gmail.com>
2018-07-19 21:31:48 +0530

    Update to requirements and prod settigs.

--- tiny/settings/production.py
+++ tiny/settings/production.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,6 +81,8 @@ DATABASES = {
     }
 }
 
+db_frm_env = dj_database_url.config()
+DATABASES['default'].update(db_frm_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
